78092320d42860f3fe9a7928f4e5f691f501ed90
P97arruda <pliniodev97@gmail.com>
2025-09-03 06:54:24 +0100

    finish project

--- orienta-obj-ex/src/application/Program.java
+++ orienta-obj-ex/src/application/Program.java
@@ -1,8 +1,25 @@
 package application;
 
-public class Program {
-    public static void main {
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Rectagle;
 
-        System.out.println("hello");
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectagle rect = new Rectagle();
+
+        System.out.println("Enter rectangle with and height: ");
+        rect.width = sc.nextDouble();
+        rect.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", rect.area());
+        System.out.printf("PERIMETER  = %.2f%n", rect.perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
+
     }
+
 }
