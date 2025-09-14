8bc4fa8da4574de637a060751bb0c989e6124112
P97arruda <pliniodev97@gmail.com>
2025-09-14 21:39:01 +0100

    New push

--- encapsulamento-Java/src/entities/Product.java
+++ encapsulamento-Java/src/entities/Product.java
@@ -1,4 +1,33 @@
 package entities;
 
 public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    // calcula valor total em estoque
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    // adiciona produtos
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    // remove produtos
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    // sobrescreve toString para exibir informações
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
 }
